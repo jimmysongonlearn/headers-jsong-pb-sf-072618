b623956c4122ff805f61d7bf58d54dd64c66aace
Adam Jonas <jonas@flatironschool.com>
2018-11-16 14:50:41 -0500

    initial commit

---
+++ test/index_test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from ipynb.fs.full.index import *
+
+class GetHeadersMessageTest(TestCase):
+
+    def test_serialize(self):
+        block_hex = '0000000000000000001237f46acddf58578a37e213d2a6edc4884a2fcad05ba3'
+        gh = GetHeadersMessage(start_block=bytes.fromhex(block_hex))
+        self.assertEqual(gh.serialize().hex(), '7f11010001a35bd0ca2f4a88c4eda6d213e2378a5758dfcd6af437120000000000000000000000000000000000000000000000000000000000000000000000000000000000')
+
+
+class HeadersMessageTest(TestCase):
+
+    def test_parse(self):
+        hex_msg = '0200000020df3b053dc46f162a9b00c7f0d5124e2676d47bbe7c5d0793a500000000000000ef445fef2ed495c275892206ca533e7411907971013ab83e3b47bd0d692d14d4dc7c835b67d8001ac157e670000000002030eb2540c41025690160a1014c577061596e32e426b712c7ca00000000000000768b89f07044e6130ead292a3f51951adbd2202df447d98789339937fd006bd44880835b67d8001ade09204600'
+        stream = BytesIO(bytes.fromhex(hex_msg))
+        headers = HeadersMessage.parse(stream)
+        self.assertEqual(len(headers.blocks), 2)
+        for b in headers.blocks:
+            self.assertEqual(b.__class__, Block)
